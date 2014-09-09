2e3b251880a0f3a6d12100ce6e83fa6596a4fc47
Deniz Eren <dee.116@gmail.com>
2014-09-09 07:22:25 +0300

    Selection sort implemented.

--- sort.go
+++ sort.go
@@ -1,15 +1,27 @@
 package sort
 
+import "fmt"
+
 func Bubble(list []int) {
 	s := len(list)
-	for s!=0 {
+	for s != 0 {
 		n := 0
-		for i:=1; i<s; i++ {
+		for i := 1; i < s; i++ {
 			if list[i-1] > list[i] {
 				list[i-1], list[i] = list[i], list[i-1]
 				n = i
 			}
 		}
 		s = n
+	}
+}
+
+func Selection(list []int) {
+	for i := 1; i < len(list); i++ {
+		for j := i; j < len(list); j++ {
+			if list[j] < list[i-1] {
+				list[i-1], list[j] = list[j], list[i-1]
+			}
+		}
 	}
 }

bd8a40f726cf458f0ca7bf8a1c141cb45828b88a
Deniz Eren <dee.116@gmail.com>
2014-11-12 21:12:16 +0200

    Queue and stack implemented.

---
+++ graph/graph.go
@@ -0,0 +1,63 @@
+package graph
+
+// Graph implementation
+type Graph struct {
+	nodes []*Node
+}
+
+type Node struct {
+	value int
+	visited bool
+	adjacents []*Node
+}
+
+func (n *Node) Equals(node *Node) bool {
+	if n.value == node.value && n.visited == node.visited {
+		for k, v := range n.adjacents {
+			if v != node.adjacents[k] {
+				return false
+			}
+		}
+		return true
+	}
+
+	return false
+}
+
+// Queue implementation
+type Queue struct {
+	data []*Node
+}
+
+func (q *Queue) Push(node *Node) {
+	q.data = append(q.data, node)
+}
+
+func (q *Queue) Pop() *Node {
+	if len(q.data) < 1 {
+		return nil
+	}
+	node := q.data[0]
+	q.data = q.data[1:]
+
+	return node
+}
+
+// Stack implementation
+type Stack struct {
+	data []*Node
+}
+
+func (s *Stack) Push(node *Node) {
+	s.data = append(s.data, node)
+}
+
+func (s *Stack) Pop() *Node {
+	if len(s.data) < 1 {
+		return nil
+	}
+	node := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+
+	return node
+}
---
+++ graph/graph_test.go
@@ -0,0 +1,59 @@
+package graph
+
+import (
+	"testing"
+	"strconv"
+)
+
+func TestQueue(t *testing.T) {
+	nodes := []*Node {
+		&Node{1, false, nil},
+		&Node{2, false, nil},
+		&Node{3, false, nil},
+		&Node{4, false, nil},
+	}
+
+	q := Queue{}
+
+	for _, v := range nodes {
+		q.Push(v)
+	}
+
+	for _, v := range nodes {
+		n := q.Pop()
+
+		if n.Equals(v) == false {
+			t.Error(
+				"Push function of queue failed.",
+				"Expected node: ", strconv.Itoa(v.value),
+				"Got node: ", strconv.Itoa(n.value),
+			)
+		}
+	}
+}
+
+func TestStack(t *testing.T) {
+	nodes := []*Node {
+		&Node{1, false, nil},
+		&Node{2, false, nil},
+		&Node{3, false, nil},
+	}
+
+	s := Stack{}
+
+	for _, v := range nodes {
+		s.Push(v)
+	}
+
+	for i:=len(nodes)-1; i>=0; i--{
+		n := s.Pop()
+
+		if n.Equals(nodes[i]) == false {
+			t.Error(
+				"Push function of queue failed.",
+				"Expected node: ", strconv.Itoa(nodes[i].value),
+				"Got node: ", strconv.Itoa(n.value),
+			)
+		}
+	}
+}
